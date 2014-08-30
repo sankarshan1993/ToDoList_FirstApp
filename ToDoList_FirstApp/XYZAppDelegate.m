85ac126b35b752dcf6c8fd33608a9ca5c3259ab9
Sankarshan Purkayastha <sankarshan.purkayastha1@gmail.com>
2014-08-31 01:54:33 +0530

    work

--- ToDoList_FirstApp/XYZAppDelegate.m
+++ ToDoList_FirstApp/XYZAppDelegate.m
@@ -12,10 +12,11 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    /* Next few lines will be deleted
+     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
     self.window.backgroundColor = [UIColor whiteColor];
-    [self.window makeKeyAndVisible];
+    [self.window makeKeyAndVisible]; */
     return YES;
 }
 
